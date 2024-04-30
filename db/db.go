9c6fb4dfc0f25ca25ec0e23d53404042417bb73e
RadonZ <supachai27102000@gmail.com>
2024-04-30 18:48:02 +0700

    create db

--- db/db.go
+++ db/db.go
@@ -8,16 +8,16 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-  DB, err := sql.Open("sqlite3", "api.db")
+	var err error
+  	DB, err = sql.Open("sqlite3", "api.db")
   
 
-  if err != nil {
-	panic("Could not connect to database")
-  }
+  	if err != nil {
+		panic("Could not connect to database")
+  	}
 
   DB.SetMaxOpenConns(10)
   DB.SetMaxIdleConns(5)
-
   createTables()
 }
 
